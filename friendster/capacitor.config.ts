f66abb7e694fc28d0d328b7925560e5b6b200ce3
Alexander Hartmann <alex.hartmann1994@gmail.com>
2021-08-08 13:03:18 +0200

    Recreated Android Project

--- friendster/capacitor.config.ts
+++ friendster/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ioni',
   appName: 'friendster',
   webDir: 'www',
   bundledWebRuntime: false
